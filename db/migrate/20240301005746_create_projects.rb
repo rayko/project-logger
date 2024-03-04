021b6edece9d3fee6957c821be082ea632015bd7
Rayko <rayko.drg@gmail.com>
2024-03-03 21:13:04 -0300

    Removing description and adding user_id for project

--- db/migrate/20240301005746_create_projects.rb
+++ db/migrate/20240301005746_create_projects.rb
@@ -2,11 +2,10 @@ class CreateProjects < ActiveRecord::Migration[7.1]
   def change
     create_table :projects do |t|
       t.string   :name,         limit: 128
-      t.text     :description,  limit: 4096
       t.string   :status,       limit: 32,   null: false
       t.boolean  :locked,       default: false
       t.datetime :locked_at
-      # t.integer  :user_id,                  null: false
+      t.integer  :user_id,                  null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -64,10 +64,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_222448) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name", limit: 128
-    t.text "description"
     t.string "status", limit: 32, null: false
     t.boolean "locked", default: false
     t.datetime "locked_at"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
