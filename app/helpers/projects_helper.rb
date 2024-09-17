fdd955e450e146f0f406deb0c9e69e9112e461e4
Rayko <rayko.drg@gmail.com>
2024-09-17 00:25:55 -0300

    Fixing update message in index

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,7 @@
 module ProjectsHelper
+  def project_last_updated_at(project)
+    return '<i>No entries</i>'.html_safe unless project.entries.any?
+
+    "Updated #{time_ago_in_words(project.entries.last.created_at)} ago"
+  end
 end
